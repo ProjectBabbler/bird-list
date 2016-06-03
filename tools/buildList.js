c982fd1978bc2ecbc3c35e9df11910dbe8842e75
Greg Schechter <diabolomaster@gmail.com>
2016-06-03 07:42:19 -0700

    Upgrade to firebase 3

--- tools/buildList.js
+++ tools/buildList.js
@@ -1,12 +1,19 @@
-var Firebase = require('firebase');
-var firebaseRef = new Firebase('https://birdlist.firebaseio.com/');
+var firebase = require('firebase');
+var config = {
+    apiKey: 'AIzaSyC8uCPB9XFgxyPDyEMHSs3jJXLOAeigzj0',
+    databaseURL: 'https://birdlist.firebaseio.com',
+    serviceAccount: {
+        project_id: 'project-3189009344311462796',
+        private_key: process.env.FIREBASE_SERVICE_KEY.replace(/\\n/g, '\n'),
+        client_email: 'private-console-key@project-3189009344311462796.iam.gserviceaccount.com',
+    }
+};
+let firebaseApp = firebase.initializeApp(config);
 var ebird = require('ebird-js');
 
-firebaseRef.authWithCustomToken(process.env.firebase).then(() => {
-    return ebird.ref.taxa();
-}).then(results => {
+ebird.ref.taxa().then(results => {
     var ps = results.map(species => {
-        return firebaseRef.child('birds').child(species.speciesCode).set(species);
+        return firebaseApp.database().ref('birds').child(species.speciesCode).set(species);
     });
     return Promise.all(ps);
 }).then(() => {
